217e26a32f9349ffe0d3872bee4e2058ec012036
stephengeorge568 <70499850+stephengeorge568@users.noreply.github.com>
2022-01-11 19:15:00 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# OT-server
+  Server application for the Operational Transformation implementation. More to come.
---
+++ src/main/java/tesseract/OTserver/OtServerApplication.java
@@ -0,0 +1,13 @@
+package tesseract.OTserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtServerApplication.class, args);
+	}
+
+}
