a8cc24d908b9b35d122c89069be393ae97a3c405
stephengeorge568 <70499850+stephengeorge568@users.noreply.github.com>
2022-01-16 22:48:21 -0600

    CORS config fix, but doesnt work with Chrome

--- src/main/java/tesseract/OTserver/controllers/StringChangeController.java
+++ src/main/java/tesseract/OTserver/controllers/StringChangeController.java
@@ -7,23 +7,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 import tesseract.OTserver.objects.StringChangeRequest;
-
+//@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 public class StringChangeController {
 
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
+
     @RequestMapping(
             consumes = {MediaType.APPLICATION_JSON_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE},
             method = RequestMethod.POST,
-            path = ""
+            path = "change"
     )
     @ResponseBody
     public void stringChange(@RequestBody StringChangeRequest request) {
         // send request to web socket subscribers
-        this.simpMessagingTemplate.convertAndSend(request);
+        System.out.println(request.getText() + " " + request.getIndex());
+        //this.simpMessagingTemplate.convertAndSend(request);
     }
 
 
